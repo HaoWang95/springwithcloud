2220db39e39ef9263a2786962ee5abd90e69b2a5
HaoWang95 <alanwang0028@gmail.com>
2021-08-09 21:20:15 +1000

    Update: create and find task update

--- src/main/java/AlanJava/SpringBootStarter/Task/controller/TaskController.java
+++ src/main/java/AlanJava/SpringBootStarter/Task/controller/TaskController.java
@@ -3,19 +3,16 @@ package AlanJava.SpringBootStarter.Task.controller;
 import AlanJava.SpringBootStarter.Task.model.Task;
 import AlanJava.SpringBootStarter.Task.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.support.NullValue;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/task/v1")
+@RequestMapping("/api/v1")
 public class TaskController {
 
     @Autowired
@@ -25,4 +22,36 @@ public class TaskController {
     public String TaskTest(){
         return "This is the index test for task controller";
     }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Task>> findAllTasks(){
+        try{
+            List<Task> taskList = new ArrayList<Task>(taskRepository.findAll());
+
+            if (taskList.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(taskList, HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/{:id}")
+    public ResponseEntity<Task> findTaskById(@PathVariable("id") long id){
+        System.out.println(id);
+        Optional<Task> task = taskRepository.findById(id);
+        return task.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Task> createTask(@RequestBody Task task){
+        try{
+            Task _task = taskRepository.save(new Task(task.getTitle(),task.getDescription()));
+            return new ResponseEntity<>(_task, HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
